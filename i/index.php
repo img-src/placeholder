591d6858d3d3e5bc687aa91ecfe0458bbdba9554
Brian K <brian@ughblah.com>
2012-05-15 16:12:20 -0500

    shorten variable names

--- i/index.php
+++ i/index.php
@@ -1,16 +1,15 @@
 <?php
-// get values from URL parameters
-$width = isset($_GET['w']) ? trim($_GET['w']) : null;
-$height = isset($_GET['h']) ? trim($_GET['h']) : null;
+require('placeholder.class.php');
+
+$w = isset($_GET['w']) ? trim($_GET['w']) : null;
+$h = isset($_GET['h']) ? trim($_GET['h']) : null;
 $bgColor = isset($_GET['bgColor']) ? strtolower(trim($_GET['bgColor'])) : null;
 $textColor = isset($_GET['textColor']) ? strtolower(trim($_GET['textColor'])) : null;
 
-require('placeholder.class.php');
-
 try {
 	$placeholder = new Placeholder();
-	$placeholder->setWidth($width);
-	$placeholder->setHeight($height);
+	$placeholder->setWidth($w);
+	$placeholder->setHeight($h);
 	if ($bgColor) $placeholder->setBgColor($bgColor);
 	if ($textColor) $placeholder->setTextColor($textColor);
 	$placeholder->render();
